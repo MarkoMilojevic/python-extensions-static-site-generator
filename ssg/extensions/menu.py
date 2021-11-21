af8c09dfcf62300cee8f4f925fbf280eea8cb6a7
Marko Milojevic <milojevicmarko92@gmail.com>
2021-11-21 23:57:18 +0100

    4.3 Collect Valid Files.

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from ssg import hooks, parsers
 
 files = []
@@ -6,3 +7,8 @@ files = []
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
+
+    for path in source.rglob("*"):
+        for parser in list(filter(valid, site_parsers)):
+            if parser.valid_file_ext(path.suffix):
+                files.append(path)
